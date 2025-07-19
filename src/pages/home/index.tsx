4b378e1b0477df9c976a47a39e66bed2f49ab4bf
paulohenriquecorrea <ph.sistemas.ti@gmail.com>
2025-07-19 15:24:04 -0400

    Finalizado conhecendo o tailwind

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 export function Home() {
     return (
-        <>
-            <h1>Home</h1>
-        </>
+        <div className="w-full h-screen bg-neutral-900">
+            <h1 className="text-white text-2xl">Home</h1>
+        </div>
     )
 }
