47b69f54dc698870b0ef9a85bc32df883a9bfa68
Paulo Henrique Correa <ph.sistemas.ti@gmail.com>
2025-07-21 16:42:17 -0400

    Criado o formulário de login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,23 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+
+import {Input} from '../../components/Input'
+import { useState, type FormEvent } from "react";
 
 export function Login() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+
+        console.log(
+            {
+                email: email,
+                password: password
+            }
+        )
+    } 
+
     return(
         <div className="flex w-full h-screen items-center justify-center flex-col">
             <Link to="/">
@@ -13,6 +30,29 @@ export function Login() {
                     </span>
                 </h1>
             </Link>
+            <form onSubmit={handleSubmit} className="w-full max-w-xl flex flex-col px-2">
+                <Input
+                    placeholder="Digite o seu email"
+                    type="email"
+                    value={email}
+                    onChange={(e) => {
+                        setEmail(e.target.value);
+                    }}
+                 />
+                <Input
+                    placeholder="*********"
+                    type="password"
+                    value={password}
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                 />
+                <button 
+                type="submit"
+                className="h-9 bg-blue-600 rounded border-0 text-lg font-medium text-white">
+                    Acessar
+                </button>
+            </form>
         </div>
     )
 }
